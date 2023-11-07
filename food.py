929c6b2ef36f0961c1486c42b4a7b5e1203485be
MariaPadilha32 <>
2023-11-07 21:59:37 +0000

    amend issues after testing

--- food.py
+++ food.py
@@ -1,15 +1,17 @@
 """
 inport the necessary functions to make sure users
 will not input incorrect information only alphabetic characters
-or numeric characters only. 
+or numeric characters only.
 """
 from utils import get_non_empty_input
 from utils import get_numeric_input
+
 
 def foodStories():
     number_food = str(get_numeric_input("Enter a Number: "))
     number1_food = str(get_numeric_input("Enter a Number: "))
     number2_food = str(get_numeric_input("Enter a Number: "))
+    ingredient_food = get_non_empty_input("Enter an Ingredient: ")
     ingredient1_food = get_non_empty_input("Enter an Ingredient: ")
     ingredient2_food = get_non_empty_input("Enter an Ingredient: ")
     number3_food = str(get_numeric_input("Enter a Number: "))
@@ -22,15 +24,27 @@ def foodStories():
 
     foodStories = {
         "guacamole_food": (
-            "\nClassic Guacamole: \nIngredients:\n" + number_food + " ripe avocados \n"
-            "1 small " + ingredient1_food + " , finely diced\n" + number1_food + " - " + number2_food + " cloves " + ingredient1_food + ", minced\n"
-            "1-2 " + ingredient2_food + " diced\nJuice of " + number3_food + "  lime(s)\n "
-            "" + condiment_food + " and pepper to taste\n...\nInstructions:\n"
-            "Cut the avocados in half, " + verb_food + "  the pit, and scoop the flesh into a " + noun_food + "\n"
-            "Mash the avocados with a fork until you reach your desired consistency (some prefer " + adjective_food + ", while others like it " + adjective1_food + ").\n"
-            "Add the diced " + ingredient1_food + ", minced " + ingredient1_food + ", and diced " + ingredient2_food +".\n"
-            "Squeeze the lime juice over the mixture and season with " + number3_food + " and pepper.\n"
-            "Mix everything together and serve with " + noun1_food + " or as a topping for tacos.\n")
+            "\nClassic Guacamole: \n"
+            "Ingredients:\n" + number_food + " ripe avocados \n"
+            "1 small " + ingredient_food + ", finely diced\n"
+            + number1_food + " - " + number2_food + " cloves "
+            + ingredient1_food + ", minced\n"
+            "1-2 " + ingredient2_food + " diced\n"
+            "Juice of " + number3_food + " lime(s)\n"
+            + condiment_food + " and pepper to taste\n"
+            "...\nInstructions:\n"
+            "Cut the avocados in half, " + verb_food +
+            " the pit, and scoop the flesh into a " + noun_food + "\n"
+            "Mash the avocados with a fork until you reach "
+            "your desired consistency (some prefer " + adjective_food +
+            ", while others like it " + adjective1_food + ").\n"
+            "Add the diced " + ingredient_food + ", minced "
+            + ingredient1_food + ", and diced " + ingredient2_food + ".\n"
+            "Squeeze the lime juice over the mixture and season with "
+            + condiment_food + " and pepper.\n"
+            "Mix everything together and serve with " + noun1_food +
+            " or as a topping for tacos.\n"
+            )
     }
 
     print(foodStories["guacamole_food"])
--- movies.py
+++ movies.py
@@ -4,6 +4,7 @@ will not input incorrect information only alphabetic characters
 """
 from utils import get_non_empty_input
 
+
 def moviesStories():
     noun_movies = get_non_empty_input("Enter a Noun: ")
     silly_word_movies = get_non_empty_input("Enter a Silly Word: ")
@@ -46,20 +47,33 @@ def moviesStories():
             "noun9_movies": noun9_movies,
             "adjective_movies": adjective_movies,
             "nightmare_on_elm_street": (
-                "\nA " + noun_movies + " on " + silly_word_movies + " Street.\n...\nThe movie begins with a mysterious "
-                + noun1_movies + " crafting a deadly glove with " + noun2_movies + " for fingers.\n" + girls_name_movies +
-                " , a girl, has a terrifying " + noun_movies + " where she's attacked by the glove-" + noun2_movies + " man."
-                "\nHer " + noun3_movies + " is mysteriously slashed during the dream.\n...\n" + girls_name_movies +
-                " shares her " + noun_movies + "  with her friends, Nancy, Rod, and Glen.\nThey've all been having similar "
-                + noun_movies + ". That night, " + girls_name_movies + " is " + verb_ed_movies + " in her dream, and her "
-                + noun4_movies + " is mirrored in the real world.\nRod is " + verb2_ed_movies + " for her " + noun5_movies + "."
-                "\n...\nNancy's mother reveals a dark secret: " + noun6_movies + "  a man named Freddy, " + surname_movies +
-                " a child " + noun7_movies + " , was " + verb_ed_movies + " by parents.\nNow, he's " + verb_ing_movies +
-                " their dreams seeking " + noun8_movies + ".\n...\nNancy tries to " + verb_movies + " her friends and face "
-                "Freddy " + surname_movies + ".\nShe sets traps and succeeds in bringing him into the real world.\nShe "
-                + verb_s_movies + " him, and her " + noun9_movies + " weakens him. Nancy defeats Freddy, and all seems well."
-                "\nHowever, in the end, a " + adjective_movies + " twist suggests that Freddy's presence still lingers.\n"
-            )
+                "\nA " + noun_movies + " on " + silly_word_movies +
+                " Street.\n...\nThe movie begins with a mysterious "
+                + noun1_movies + " crafting a deadly glove with "
+                + noun2_movies + " for fingers.\n" + girls_name_movies +
+                " , a girl, has a terrifying " + noun_movies +
+                " where she's attacked by the glove-" + noun2_movies + " man."
+                "\nHer " + noun3_movies +
+                " is mysteriously slashed during the dream.\n...\n"
+                + girls_name_movies + " shares her " + noun_movies +
+                "  with her friends, Nancy, Rod, and Glen.\n"
+                "They've all been having similar " + noun_movies +
+                ". That night, " + girls_name_movies + " is "
+                + verb_ed_movies + " in her dream, and her "
+                + noun4_movies + " is mirrored in the real world.\nRod is "
+                + verb2_ed_movies + " for her " + noun5_movies + "."
+                "\n...\nNancy's mother reveals a dark secret: "
+                + noun6_movies + "  a man named Freddy, " + surname_movies +
+                " a child " + noun7_movies + " , was " + verb_ed_movies +
+                " by parents.\nNow, he's " + verb_ing_movies +
+                " their dreams seeking " + noun8_movies +
+                ".\n...\nNancy tries to " + verb_movies +
+                " her friends and face Freddy " + surname_movies +
+                ".\nShe sets traps and brings him into the real world.\nShe "
+                + verb_s_movies + " him, and her " + noun9_movies +
+                " weakens him. Nancy defeats Freddy, and all seems well."
+                "\nHowever, in the end, a " + adjective_movies +
+                " twist suggests that Freddy's presence still lingers.\n")
         }
     print(moviesStories["nightmare_on_elm_street"])
 
